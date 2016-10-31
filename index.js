0881326cdaac4f0011ef1fdb63ca283fddfc01c0
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-10-31 16:45:39 +0700

    flatten kelar

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ console.log(cater.count([1,2,3]))
 
 console.log("head")
 console.log(cater.head([1,2,3]))
+
+console.log("tail")
+console.log(cater.tail([2,3]))
+
+console.log("flatten")
+console.log(cater.flatten([1, [2,3], 4, [5, [6,[7,8,[9,10]]]]]))
