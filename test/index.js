faa882487b917ed59c9b9265b79c3e9c6daad491
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-10-31 16:59:54 +0700

    testing semua kelar

--- test/index.js
+++ test/index.js
@@ -1,5 +1,34 @@
 const mocha = require('mocha')
-const chai = require('chai')
+const expect = require('chai').expect
 const cater = require('../lib/cater')
 
-expect(console.log(cater.isEmpty([]))).to.be.true
+describe("Test isEmpty", () => {
+    it("returns true if cater is empty", () => {
+        expect(cater.isEmpty([])).to.be.true
+    })
+})
+
+describe("Test count", () => {
+    it("returns true if count is 3", () => {
+        expect(cater.count([1,2,3])).equal(3)
+    })
+})
+
+describe("Test head", () => {
+    it("returns true if head is 1", () => {
+        expect(cater.head([1,2,3])).equal(1)
+    })
+})
+
+describe("Test tail", () => {
+    it("returns true if tail is 3", () => {
+        expect(cater.tail([1,2,3])).equal(3)
+    })
+})
+
+describe("Test flatten", () => {
+    it("returns true if flatten is [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", () => {
+        expect(cater.flatten([1, [2,3], 4, [5, [6,[7,8,[9,10]]]]])).eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    })
+})
+
