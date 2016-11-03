d16b4e4798a832f4ee6b28eaf96ec9baf69659e0
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-03 13:28:40 +0700

    uncomment cater.test.js

--- test/cater.test.js
+++ test/cater.test.js
@@ -6,41 +6,41 @@ const cater = require('../lib/cater');
 
 // console.log(cater.tail([]));
 
-// describe('#isEmpty()', function(){
-//   it('it should return true if empty', function(){
-//     /* cara equal */
-//     assert.equal(true, cater.isEmpty([1]));
-//
-//     /* cara expect */
-//     // expect(cater.isEmpty([])).to.be.true
-//   })
-// })
+describe('#isEmpty()', function(){
+  it('it should return true if empty', function(){
+    /* cara equal */
+    assert.equal(true, cater.isEmpty([]));
 
-// describe('#count()', function(){
-//   it('it should return array length', function(){
-//     /* cara equal */
-//     assert.equal(2, cater.count([1,2]));
-//
-//     /* cara expect */
-//     // expect(cater.count([])).to.be.2
-//   })
-// })
+    /* cara expect */
+    // expect(cater.isEmpty([])).to.be.true
+  })
+})
+
+describe('#count()', function(){
+  it('it should return array length', function(){
+    /* cara equal */
+    assert.equal(2, cater.count([1,2]));
+
+    /* cara expect */
+    // expect(cater.count([])).to.be.2
+  })
+})
 
 
-// describe('#head()', function(){
-//   it('it should return value of index 0', function(){
-//     /* cara equal */
-//     assert.equal(2, cater.head([2,1]));
-//     /* cara expect */
-//     // expect(cater.head([2,1])).to.be.2
-//   })
-// })
+describe('#head()', function(){
+  it('it should return value of index 0', function(){
+    /* cara equal */
+    assert.equal(2, cater.head([2,1]));
+    /* cara expect */
+    // expect(cater.head([2,1])).to.be.2
+  })
+})
 
-// describe('#tail()', function(){
-//   it('it should return array from index of 1 to array.length', function(){
-//     expect(cater.tail([2,1,4])).eql([1,4])
-//   })
-// })
+describe('#tail()', function(){
+  it('it should return array from index of 1 to array.length', function(){
+    expect(cater.tail([2,1,4])).eql([1,4])
+  })
+})
 
 describe('#flatten()', function(){
   it('it should return array from index of 1 to array.length', function(){
