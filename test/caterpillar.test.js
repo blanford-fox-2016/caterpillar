0d9c226d2ea4fb2bb91862e11bb79e1f32a445bc
Septian A Tama <sfirst.tama@gmail.com>
2016-11-06 13:18:52 +0700

    add testing script for function head

--- test/caterpillar.test.js
+++ test/caterpillar.test.js
@@ -5,6 +5,9 @@ const assert = chai.assert;
 const caterpillar = require('../caterpillar');
 const isEmpty = caterpillar.isEmpty;
 const count = caterpillar.count;
+const head = caterpillar.head;
+const tail = caterpillar.tail;
+const flatten = caterpillar.flatten;
 
 describe('isEmpty', () => {
   it('should return true if the array is empty', () => {
@@ -21,5 +24,11 @@ describe('isEmpty', () => {
 describe('count', () => {
   it('should return the numbers of value, inside the array', () => {
     assert.equal(5, count([1, 2, 3, 4, 5]));
+  })
+})
+
+describe('head', () => {
+  it('should return the first value of an array', () => {
+    assert.equal(1, head([1, 2, 3, 4, 5]));
   })
 })
